6caee21523df5ac33b0f2601824c68d2b91802ae
JAKellum <jason@ignitewithus.com>
2015-04-05 03:25:07 -0400

    AIGA compare section

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,3 +27,10 @@ $(document).ready(function() {
 });
 
 $(function(){$("a[href*=#]:not([href=#])").click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var e=$(this.hash);e=e.length?e:$("[name="+this.hash.slice(1)+"]");if(e.length){$("html,body").animate({scrollTop:e.offset().top},1e3);return false}}})})
+
+$('select.div-toggler').change(function(){
+    var target = $(this).data('target');
+    $(target).children().addClass('hide');
+    var show = $("option:selected", this).data('show');
+    $(show).removeClass('hide');
+});
