d913dfb74bc031797e7720b7c3b3e4733f3b1967
JAKellum <jason@ignitewithus.com>
2015-06-20 20:03:30 -0400

    smoothState, Rm subnav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,9 +14,40 @@
 //= require selectivizr
 //= require jquery
 //= require jquery_ujs
+//= require smoothState
 //= require windows.min
 
 $(document).ready();
+
+$(function(){
+  'use strict';
+  var options = {
+    prefetch: true,
+    cacheLength: 2,
+    onStart: {
+      duration: 250, // Duration of our animation
+      render: function ($container) {
+        // Add your CSS animation reversing class
+        $container.addClass('is-exiting');
+
+        // Restart your animation
+        smoothState.restartCSSAnimations();
+      }
+    },
+    onReady: {
+      duration: 0,
+      render: function ($container, $newContent) {
+        // Remove your CSS animation reversing class
+        $container.removeClass('is-exiting');
+
+        // Inject the new content
+        $container.html($newContent);
+
+      }
+    }
+  },
+  smoothState = $('#main').smoothState(options).data('smoothState');
+});
 
 $('#nav-icon').click(function(){
 	  $('.overlay').slideToggle( 'show' );
