05f34b86c3814894a9eb3bbbd3398df017afa535
JAKellum <jason@ignitewithus.com>
2015-09-25 17:33:29 -0400

    Multiple div toggles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
   		 $(this).toggleClass('open');
   	});
 
-    if ($('.aiga').length > 0) {
+    if ($('.aiga, .resumebot').length > 0) {
 
       $('select.div-toggler').change(function() {
         var target = $(this).data('target');
